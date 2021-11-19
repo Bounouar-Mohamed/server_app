10684e30a7f0d82cf6860baa945b3889016a9e4d
Bounouar Mohamed <momobounouar13@gmail.com>
2021-11-19 01:11:58 +0100

    Get data.json

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
+app.use (bodyParser.json());
 const fs = require('fs');
 
 
-
-let user = {
+    let user = {
     firstName : 'Mohamed',
     lastName : 'Bounouar'
  };
+    
 
 const port = 3000;
 app.listen(port, () => console.log('server running...'))
@@ -19,25 +19,28 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
-
-  
+/* 
 app.get('/users', (req, res) => {
+
     res.send(user);
 });
+*/
+app.get('/users', (req, res) => {
+    
+    res.sendFile(__dirname +'/data.json');
+});
+ 
 
-app.post("/users", function (req, res) {
+app.post("/users", function (req, res,) {
 
     console.log(req.body.data);
 
-    
-
-fs.writeFile("./data.txt", req.body.data, function(err) {
+     
+    fs.appendFile("./data.json",JSON.stringify({ Data : req.body.data }) , function(err) {
     if(err) {
         return console.log(err);
     }
     console.log("The file was saved!");
-});
-});
 
-// Or
-    
+});
+});
